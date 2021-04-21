1464c0acc2574340c9b63a6e7761713a127e6d4b
niloysikdar <niloysikdar30@gmail.com>
2021-04-22 00:53:12 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+import discord
+import os
+
+load_dotenv()
+token = os.getenv('TOKEN')
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if ("joke" in message.content):
+            await message.channel.send("Wait and see")
+
+
+client = MyClient()
+client.run(token)
